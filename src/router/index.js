0e8598c8e3c88b4e03237229f2e40d165e9bffe7
Wave <372969383@qq.com>
2017-05-23 10:11:02 +0800

    pwa

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Start from '@/components/Start'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/start',
+      name: 'Start',
+      component: Start
     }
   ]
 })
